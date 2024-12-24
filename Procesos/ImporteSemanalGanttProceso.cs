bd36941fc1684bf61701ce63f56638a0e7b2f3b2
CFloresProcomi <146362787+CFloresProcomi@users.noreply.github.com>
2024-12-24 09:27:06 -0600

    Importe Semanal Gantt por tipo de insumo
    Importe Semanal Gantt por tipo de insumo

--- Procesos/ImporteSemanalGanttProceso.cs
+++ Procesos/ImporteSemanalGanttProceso.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Components.Forms;
+﻿using ERP_TECKIO.Modelos;
+using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.EntityFrameworkCore;
 using System.Data;
 using System.Globalization;
@@ -9,19 +10,22 @@ namespace ERP_TECKIO.Procesos
     {
         private readonly T _dbContex;
         ProgramacionEstimadaGanttProceso<T> _programacionEstimadaGanttProceso;
+        PrecioUnitarioProceso<T> _precioUnitarioProceso;
         public ImporteSemanalGanttProceso(
             T dbContex,
-            ProgramacionEstimadaGanttProceso<T> programacionEstimadaGanttProceso
+            ProgramacionEstimadaGanttProceso<T> programacionEstimadaGanttProceso,
+            PrecioUnitarioProceso<T> precioUnitarioProceso
             ) {
             _dbContex = dbContex;
             _programacionEstimadaGanttProceso = programacionEstimadaGanttProceso;
+            _precioUnitarioProceso = precioUnitarioProceso;
         }
 
         public async Task<List<ImporteSemanalDTO>> ImporteSemanal(int IdProyecto) { //True en EsSabado o EsDomingo = no se trabaja
             var db = _dbContex;
 
             var registros = await _programacionEstimadaGanttProceso.ObtenerProgramacionEstimadaXIdProyecto(IdProyecto, db);
-            if (registros.Count() <= 0) { 
+            if (registros.Count() <= 0) {
                 return new List<ImporteSemanalDTO>();
             }
 
@@ -63,8 +67,8 @@ namespace ERP_TECKIO.Procesos
                             fI = fI.AddDays(1);
                         }
                     }
-                    
-                    var costoDia = z.Importe / (dias.Days-domingos-sabados);
+
+                    var costoDia = z.Importe / (dias.Days - domingos - sabados);
                     int diasSemanas = 7;
                     if (DomingoLaboral)
                     {
@@ -74,10 +78,10 @@ namespace ERP_TECKIO.Procesos
                     {
                         diasSemanas--;
                     }
-                    bool esInicio =(int) z.Start.DayOfWeek == 1;
+                    bool esInicio = (int)z.Start.DayOfWeek == 1;
 
                     if (esInicio) {
-                        bool esFin = (int) z.End.DayOfWeek == 0;
+                        bool esFin = (int)z.End.DayOfWeek == 0;
                         if (esFin) {
                             if (startOfWeek >= z.Start && endOfWeek <= z.End)
                             {
@@ -88,9 +92,9 @@ namespace ERP_TECKIO.Procesos
                             else {
                                 continue;
                             }
-                        }else {
+                        } else {
                             var indice2 = (int)z.End.DayOfWeek;
-                            var nuevaFechaFin = z.End.AddDays(7-indice2);
+                            var nuevaFechaFin = z.End.AddDays(7 - indice2);
                             if (startOfWeek >= z.Start && endOfWeek <= nuevaFechaFin)
                             {
                                 decimal total = 0;
@@ -108,11 +112,12 @@ namespace ERP_TECKIO.Procesos
                                 SubTotalSemana = SubTotalSemana + total;
                                 continue;
                             }
-                            else {
+                            else 
+                            {
                                 continue;
                             }
                         }
-                    }   
+                    }
                     else
                     {
                         var nuevaFecha = z.Start.AddDays(((int)(z.Start.DayOfWeek) * -1) + 1);
@@ -150,7 +155,8 @@ namespace ERP_TECKIO.Procesos
                                 SubTotalSemana = SubTotalSemana + total;
                                 continue;
                             }
-                            else {
+                            else 
+                            {
                                 continue;
                             }
                         }
@@ -170,10 +176,282 @@ namespace ERP_TECKIO.Procesos
                 startOfWeek = startOfWeek.AddDays(7);
                 endOfWeek = startOfWeek.AddDays(6);
                 EndSemana = cul.Calendar.GetWeekOfYear(endOfWeek, CalendarWeekRule.FirstDay, DayOfWeek.Monday);
-                
+
             }
 
             return semanas;
+        }
+
+        public async Task<ImportesSemanalesPorTipoDTO> ObtenerTotalesPorTipo(int IdProyecto)
+        {
+            var db = _dbContex;
+
+            var registros = await _programacionEstimadaGanttProceso.ObtenerProgramacionEstimadaXIdProyecto(IdProyecto, db);
+            if (registros.Count <= 0)
+            {
+                return new ImportesSemanalesPorTipoDTO();
+            }
+            var PreciosUnitarios = await _precioUnitarioProceso.ObtenerPrecioUnitarioSinEstructurar(IdProyecto);
+
+            DateTime fechaInicial = registros.Min(z => z.Start);
+            DateTime fechaFinal = registros.Max(z => z.End);
+
+            var indice = (int)fechaInicial.DayOfWeek;
+            DateTime startOfWeek = fechaInicial.AddDays(((int)(fechaInicial.DayOfWeek) * -1) + 1);
+            DateTime endOfWeek = startOfWeek.AddDays(6);
+
+            CultureInfo cul = CultureInfo.CurrentCulture;
+
+            var EndSemana = cul.Calendar.GetWeekOfYear(endOfWeek, CalendarWeekRule.FirstDay, DayOfWeek.Monday);
+            var semanasMDO = new List<ImporteSemanalPorTipoDTO>();
+            var semanasMaterial = new List<ImporteSemanalPorTipoDTO>();
+            var semanasEquipo = new List<ImporteSemanalPorTipoDTO>();
+
+            var DomingoLaboral = registros.First().EsDomingo;
+            var SabadoLaboral = registros.First().EsSabado;
+            var ProgramacionesExistentesEnIntervaloDeTiempo = registros.Where(z => z.TipoPrecioUnitario == 1);
+            var ImportesSemanales = new ImportesSemanalesPorTipoDTO();
+            while (startOfWeek < fechaFinal)
+            {
+                decimal TotalMDOSemana = 0;
+                decimal TotalEquipoSemana = 0;
+                decimal TotalMaterialSemana = 0;
+                foreach (ProgramacionEstimadaGanttDTO registro in ProgramacionesExistentesEnIntervaloDeTiempo)
+                {
+                    decimal TotalMDO = 0;
+                    decimal TotalEquipo = 0;
+                    decimal TotalMaterial = 0;
+                    var explosionDeInsumos = await _precioUnitarioProceso.ObtenerExplosionDeInsumoXConcepto(registro.IdPrecioUnitario);
+                    foreach(InsumoParaExplosionDTO registroInsumo in explosionDeInsumos)
+                    {
+                        int IdPadre = 0;
+                        if(registroInsumo.idTipoInsumo == 10000)
+                        {
+                            decimal totalAuxiliar = registroInsumo.Importe;
+                            var PrecioUnitarioRegistro = PreciosUnitarios.Where(z => z.Id == registro.IdPrecioUnitario).FirstOrDefault();
+                            IdPadre = PrecioUnitarioRegistro.IdPrecioUnitarioBase;
+                            while(IdPadre != 0)
+                            {
+                                var padre = PreciosUnitarios.Where(z => z.Id == registro.IdPrecioUnitario).FirstOrDefault();
+                                totalAuxiliar = totalAuxiliar * padre.Cantidad;
+                                IdPadre = padre.IdPrecioUnitarioBase;
+                            }
+                            TotalMDO = TotalMDO + totalAuxiliar;
+                        }
+                        if(registroInsumo.idTipoInsumo == 10003)
+                        {
+                            decimal totalAuxiliar = registroInsumo.Importe;
+                            var PrecioUnitarioRegistro = PreciosUnitarios.Where(z => z.Id == registro.IdPrecioUnitario).FirstOrDefault();
+                            IdPadre = PrecioUnitarioRegistro.IdPrecioUnitarioBase;
+                            while (IdPadre != 0)
+                            {
+                                var padre = PreciosUnitarios.Where(z => z.Id == registro.IdPrecioUnitario).FirstOrDefault();
+                                totalAuxiliar = totalAuxiliar * padre.Cantidad;
+                                IdPadre = padre.IdPrecioUnitarioBase;
+                            }
+                            TotalEquipo = TotalEquipo + totalAuxiliar;
+                        }
+                        if (registroInsumo.idTipoInsumo == 10004)
+                        {
+                            decimal totalAuxiliar = registroInsumo.Importe;
+                            var PrecioUnitarioRegistro = PreciosUnitarios.Where(z => z.Id == registro.IdPrecioUnitario).FirstOrDefault();
+                            IdPadre = PrecioUnitarioRegistro.IdPrecioUnitarioBase;
+                            while (IdPadre != 0)
+                            {
+                                var padre = PreciosUnitarios.Where(z => z.Id == registro.IdPrecioUnitario).FirstOrDefault();
+                                totalAuxiliar = totalAuxiliar * padre.Cantidad;
+                                IdPadre = padre.IdPrecioUnitarioBase;
+                            }
+                            TotalMaterial = TotalMaterial + totalAuxiliar;
+                        }
+                    }
+                    foreach(var z in registros)
+                    {
+                        DateTime fI = z.Start;
+                        DateTime fF = z.End;
+                        int domingos = 0;
+                        int sabados = 0;
+                        var dias = z.End - z.Start;
+                        if (DomingoLaboral)
+                        {
+                            while (fI <= fF)
+                            {
+                                if (fI.DayOfWeek == DayOfWeek.Sunday)
+                                {
+                                    domingos++;
+                                }
+                                if (fI.DayOfWeek == DayOfWeek.Saturday)
+                                {
+                                    sabados++;
+                                }
+                                fI = fI.AddDays(1);
+                            }
+                        }
+                        var costoDiaMDO = TotalMDO / (dias.Days - domingos - sabados);
+                        var costoDiaEquipo = TotalEquipo / (dias.Days - domingos - sabados);
+                        var costoDiaMaterial = TotalMaterial / (dias.Days - domingos - sabados);
+                        int diasSemanas = 7;
+                        if (DomingoLaboral)
+                        {
+                            diasSemanas--;
+                        }
+                        if (SabadoLaboral)
+                        {
+                            diasSemanas--;
+                        }
+                        bool esInicio = (int)z.Start.DayOfWeek == 1;
+
+                        if (esInicio)
+                        {
+                            bool esFin = (int)z.End.DayOfWeek == 0;
+                            if (esFin)
+                            {
+                                if (startOfWeek >= z.Start && endOfWeek <= z.End)
+                                {
+                                    decimal totalMDO = diasSemanas * costoDiaMDO;
+                                    decimal totalEquipo = diasSemanas * costoDiaEquipo;
+                                    decimal totalMaterial = diasSemanas * costoDiaMaterial;
+                                    TotalMDOSemana = TotalMDOSemana + totalMDO;
+                                    TotalEquipoSemana = TotalEquipoSemana + totalEquipo;
+                                    TotalMaterialSemana = TotalMaterialSemana + totalMaterial;
+                                    continue;
+                                }
+                                else
+                                {
+                                    continue;
+                                }
+                            }
+                            else
+                            {
+                                var indice2 = (int)z.End.DayOfWeek;
+                                var nuevaFechaFin = z.End.AddDays(7 - indice2);
+                                if (startOfWeek >= z.Start && endOfWeek <= nuevaFechaFin)
+                                {
+                                    decimal totalMDO = 0;
+                                    decimal totalEquipo = 0;
+                                    decimal totalMaterial = 0;
+                                    if (EndSemana == cul.Calendar.GetWeekOfYear(nuevaFechaFin, CalendarWeekRule.FirstDay, DayOfWeek.Monday))
+                                    {
+                                        if (indice2 == 6 && SabadoLaboral)
+                                        {
+                                            indice2 = 5;
+                                        }
+                                        totalMDO = indice2 * costoDiaMDO;
+                                        totalEquipo = indice2 * costoDiaEquipo;
+                                        totalMaterial = indice2 * costoDiaMaterial;
+                                    }
+                                    else
+                                    {
+                                        totalMDO = diasSemanas * costoDiaMDO;
+                                        totalEquipo = diasSemanas * costoDiaEquipo;
+                                        totalMaterial = diasSemanas * costoDiaMaterial;
+                                    }
+                                    TotalMDOSemana = TotalMDOSemana + totalMDO;
+                                    TotalEquipoSemana = TotalEquipoSemana + totalEquipo;
+                                    TotalMaterialSemana = TotalMaterialSemana + totalMaterial;
+                                    continue;
+                                }
+                                else
+                                {
+                                    continue;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            var nuevaFecha = z.Start.AddDays(((int)(z.Start.DayOfWeek) * -1) + 1);
+                            bool esFin = (int)z.End.DayOfWeek == 0;
+                            if (esFin)
+                            {
+                                if (startOfWeek >= nuevaFecha && endOfWeek <= z.End)
+                                {
+                                    decimal totalMDO = diasSemanas * costoDiaMDO;
+                                    decimal totalEquipo = diasSemanas * costoDiaEquipo;
+                                    decimal totalMaterial = diasSemanas * costoDiaMaterial;
+                                    TotalMDOSemana = TotalMDOSemana + totalMDO;
+                                    TotalEquipoSemana = TotalEquipoSemana + totalEquipo;
+                                    TotalMaterialSemana = TotalMaterialSemana + totalMaterial;
+                                    continue;
+                                }
+                                else
+                                {
+                                    continue;
+                                }
+                            }
+                            else
+                            {
+                                var indice2 = (int)z.End.DayOfWeek;
+                                var nuevaFechaFin = z.End.AddDays(7 - indice2);
+                                if (startOfWeek >= nuevaFecha && endOfWeek <= nuevaFechaFin)
+                                {
+                                    decimal totalMDO = 0;
+                                    decimal totalEquipo = 0;
+                                    decimal totalMaterial = 0;
+                                    if (EndSemana == cul.Calendar.GetWeekOfYear(nuevaFechaFin, CalendarWeekRule.FirstDay, DayOfWeek.Monday))
+                                    {
+                                        if (indice2 == 6 && SabadoLaboral)
+                                        {
+                                            indice2 = 5;
+                                        }
+                                        totalMDO = indice2 * costoDiaMDO;
+                                        totalEquipo = indice2 * costoDiaEquipo;
+                                        totalMaterial = indice2 * costoDiaMaterial;
+                                    }
+                                    else
+                                    {
+                                        totalMDO = diasSemanas * costoDiaMDO;
+                                        totalEquipo = diasSemanas * costoDiaEquipo;
+                                        totalMaterial = diasSemanas * costoDiaMaterial;
+                                    }
+                                    TotalMDOSemana = TotalMDOSemana + totalMDO;
+                                    TotalEquipoSemana = TotalEquipoSemana + totalEquipo;
+                                    TotalMaterialSemana = TotalMaterialSemana + totalMaterial;
+                                    continue;
+                                }
+                                else
+                                {
+                                    continue;
+                                }
+                            }
+                        }
+                    }
+                    semanasMDO.Add(new ImporteSemanalPorTipoDTO()
+                    {
+                        NumeroSemana = EndSemana,
+                        Anio = startOfWeek.Year,
+                        FechaInicio = startOfWeek,
+                        FechaFin = endOfWeek,
+                        Total = TotalMDOSemana,
+                        TotalConFormato = String.Format("{0:#,##0.00}", TotalMDOSemana)
+
+                    });
+                    semanasMaterial.Add(new ImporteSemanalPorTipoDTO()
+                    {
+                        NumeroSemana = EndSemana,
+                        Anio = startOfWeek.Year,
+                        FechaInicio = startOfWeek,
+                        FechaFin = endOfWeek,
+                        Total = TotalMaterialSemana,
+                        TotalConFormato = String.Format("{0:#,##0.00}", TotalMaterialSemana)
+                    });
+                    semanasEquipo.Add(new ImporteSemanalPorTipoDTO()
+                    {
+                        NumeroSemana = EndSemana,
+                        Anio = startOfWeek.Year,
+                        FechaInicio = startOfWeek,
+                        FechaFin = endOfWeek,
+                        Total = TotalEquipoSemana,
+                        TotalConFormato = String.Format("{0:#,##0.00}", TotalEquipoSemana)
+                    });
+                    startOfWeek = startOfWeek.AddDays(7);
+                    endOfWeek = startOfWeek.AddDays(6);
+                    EndSemana = cul.Calendar.GetWeekOfYear(endOfWeek, CalendarWeekRule.FirstDay, DayOfWeek.Monday);
+                }
+                ImportesSemanales.semanasMDO = semanasMDO;
+                ImportesSemanales.semanasMaterial = semanasMaterial;
+                ImportesSemanales.semanasEquipo = semanasEquipo;
+            }
+            return ImportesSemanales;
         }
     }
 }
