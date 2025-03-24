04ff9036f476d9e24638147cc74fde96c49476a2
Kuri <cflores@procomi.com>
2025-03-24 17:21:08 -0600

    correccion fsr

--- Procesos/FactorSalarioRealProceso.cs
+++ Procesos/FactorSalarioRealProceso.cs
@@ -596,7 +596,7 @@ namespace ERP_TECKIO
                 var nuevoFSR = new FactorSalarioRealDTO();
                 nuevoFSR.IdProyecto = nuevoDia.IdProyecto;
                 nuevoFSR.PorcentajeFsr = 1;
-                nuevoFSR = await _FSRService.CrearYObtener(FSR);
+                nuevoFSR = await _FSRService.CrearYObtener(nuevoFSR);
                 FSR = nuevoFSR;
             }
             var FSI = await ObtenerFSI(nuevoDia.IdProyecto);
