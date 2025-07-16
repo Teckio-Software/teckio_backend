3c39d0cfbde44e190a92b89117019af3de0bcf12
xYairrrx <yalmanza@rfacil.com>
2025-07-16 16:44:10 -0600

    config de encoding

--- Procesos/PrecioUnitarioProceso.cs
+++ Procesos/PrecioUnitarioProceso.cs
@@ -23,6 +23,7 @@ using System.Data;
 using System.Linq.Expressions;
 using System.Text;
 using System.Text.Json;
+System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
 
 namespace ERP_TECKIO
 {
